d3755e1bd9a27902a0ed6702cf77fd3c253dbd69
任鉴非 <renjianfei@hualala.com>
2017-04-11 18:39:59 +0800

    获取会员信息（修改：封装code和massage部分）

--- service/src/main/java/com/hualala/app/wechat/impl/MemberInfoRpcServiceImpl.java
+++ service/src/main/java/com/hualala/app/wechat/impl/MemberInfoRpcServiceImpl.java
@@ -3,6 +3,7 @@ package com.hualala.app.wechat.impl;
 import com.alibaba.fastjson.JSONObject;
 import com.hualala.app.wechat.MemberInfoRpcService;
 import com.hualala.app.wechat.service.MemberInfoService;
+import com.hualala.app.wechat.util.ResultUtil;
 import com.hualala.app.wechat.util.WechatNameConverterUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,7 @@ import java.util.Map;
 
 /**
  * Created by renjianfei on 2017/4/10.
+ * 获取会员信息实现类
  */
 
 @Service
@@ -21,23 +23,17 @@ public class MemberInfoRpcServiceImpl implements MemberInfoRpcService{
 
     @Override
     public MemberInfoResData queryMemberInfo(MemberInfoQueryReqData reqData) throws Exception {
-
         String appID = reqData.getAppID();
         String appSecret = reqData.getAppSecret();
         String cardId = reqData.getCardId();
         String code = reqData.getCode();
-
         HashMap<String, Object> params = new HashMap<>();
         params.put("cardId",cardId);
         params.put("code",code);
-
         Map<String, Object> map = WechatNameConverterUtil.convertToDBStyle(params);
-
         memberInfoService.setAppId(appID);
         memberInfoService.setAppSecret(appSecret);
         JSONObject jsonObject = memberInfoService.visitWeChat(map);
-
-        MemberInfoResData memberInfoResData = JSONObject.parseObject(jsonObject.toJSONString(), MemberInfoResData.class);
-        return memberInfoResData;
+        return ResultUtil.getResultInfoBean(jsonObject,MemberInfoResData.class);
     }
 }
