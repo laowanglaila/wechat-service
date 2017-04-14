8e6e2bde16d404d1239fd964c16c1ed30d5a2448
徐 凯 <xukai@hualala.com>
2017-04-14 11:31:37 +0800

    增加微信模板消息接口

---
+++ service/src/test/java/com/hualala/app/wechat/service/WechatTemplateServiceTest.java
@@ -0,0 +1,32 @@
+package com.hualala.app.wechat.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * Created by xkia on 2017/4/11.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WechatTemplateServiceTest {
+
+
+//    @Autowired
+//    WechatTemplateService wechatTemplateService;
+//
+//    @Test
+//    public void getTemplate() throws Exception {
+//        wechatTemplateService.getTemplate("doulaofang_caicai",)
+//    }
+//
+//    @Test
+//    public void initTemplate() throws Exception {
+//    }
+
+}
