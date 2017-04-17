d2cc73ed9b0b52c156c8b6fe69421a25c7e46ae1
徐 凯 <xukai@hualala.com>
2017-04-17 12:16:19 +0800

    增加查询具有api接口权限的公众号编码

--- service/src/main/java/com/hualala/app/wechat/mapper/WechatMpMapper.java
+++ service/src/main/java/com/hualala/app/wechat/mapper/WechatMpMapper.java
@@ -2,6 +2,8 @@ package com.hualala.app.wechat.mapper;
 
 import com.hualala.app.wechat.model.WechatMpModel;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
 import java.util.List;
 import java.util.Map;
 @Mapper
@@ -15,4 +17,12 @@ public interface WechatMpMapper {
 
 	int queryCount(Map<String, Object> params);
 
+	/**
+	 * 获取巨头api权限的公众号 shopID暂时保留，
+	 * @param groupID
+	 * @param brandID
+	 * @param shopID
+	 * @return
+	 */
+	String queryMpIDAuth(@Param("groupID") long groupID, @Param("brandID") long brandID, @Param("shopID") long shopID);
 }
--- service/src/main/java/com/hualala/app/wechat/service/MpInfoService.java
+++ service/src/main/java/com/hualala/app/wechat/service/MpInfoService.java
@@ -1,9 +1,9 @@
 package com.hualala.app.wechat.service;
 
 import com.hualala.app.wechat.mapper.WechatMpMapper;
-import com.hualala.app.wechat.model.WechatMpModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import java.util.List;
 import java.util.Map;
@@ -19,18 +19,34 @@ public class MpInfoService {
 
     /**
      * query mpInfo by mpID or appID
+     *
      * @param param
      */
-    public Map<String,Object> queryMpInfo(Map<String,Object> param) {
+    public Map<String, Object> queryMpInfo(Map<String, Object> param) {
 
-
-        List<Map<String,Object>> list = wechatMpMapper.queryByParams(param);
-        if(list.size() == 0){
+        List<Map<String, Object>> list = wechatMpMapper.queryByParams(param);
+        if (list.size() == 0) {
             return null;
         }
         return list.get(0);
     }
 
 
+    /**
+     * 获取具有api权限的公众号
+     *
+     * @param groupID
+     * @param brandID
+     * @param shopID
+     * @return
+     */
+    public String queryMpIDAuth(long groupID, long brandID, long shopID) {
+        String mpID = wechatMpMapper.queryMpIDAuth(groupID, brandID, shopID);
+
+        if (StringUtils.isEmpty(mpID)) {
+            return null;
+        }
+        return mpID;
+    }
 
 }
