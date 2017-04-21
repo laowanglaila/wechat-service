37f18abf2c37879996f726654bab641b3c0d5fe8
任鉴非 <renjianfei@hualala.com>
2017-04-21 16:22:38 +0800

    名称转换工具（递归转换list和map的value值）

--- service/src/test/java/com/hualala/app/wechat/WechatNameConverterUtilTest.java
+++ service/src/test/java/com/hualala/app/wechat/WechatNameConverterUtilTest.java
@@ -3,6 +3,9 @@ package com.hualala.app.wechat;
 import com.hualala.app.wechat.util.WechatNameConverterUtil;
 import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by renjianfei on 2017/4/10.
  */
@@ -18,6 +21,30 @@ public class WechatNameConverterUtilTest {
         String javaFieldNames = "userHomeAddr";
         String dbKey = WechatNameConverterUtil.getDBKey(javaFieldNames);
         System.out.println(dbKey);
+    }
+    @Test
+    public void test1() {
+        Map<String, Object> map1 = new HashMap<>();
+        Map<String, Object> map2 = new HashMap<>();
+        Map<String, Object> map3 = new HashMap<>();
+        String dbKeys = "max_increase_bonus";
+        map3.put("max_increase_bonus",dbKeys);
+        map2.put("max_increase_bonus",map3);
+        map1.put("max_increase_bonus",map2);
+        Map Key = WechatNameConverterUtil.convertToJavaStyle(map1);
+        System.out.println(Key);
+        System.out.println("-----------------------------------");
+
+        String javaKey = "maxIncreaseBonus";
+        Map<String, Object> map4 = new HashMap<>();
+        Map<String, Object> map5 = new HashMap<>();
+        Map<String, Object> map6 = new HashMap<>();
+        map6.put("maxIncreaseBonus",javaKey);
+        map5.put("maxIncreaseBonus",map6);
+        map4.put("maxIncreaseBonus",map5);
+        Map Key2 = WechatNameConverterUtil.convertToDBStyle(map4);
+        System.out.println(Key2);
+        System.out.println("-----------------------------------");
     }
 
 }
