1ff918fcd6d10a7108d8ee5e420bfff8331b9dc0
任鉴非 <renjianfei@hualala.com>
2017-05-09 09:24:35 +0800

    导入小卖铺卡券信息Test

--- service/src/test/java/com/hualala/app/wechat/impl/card/CardSyncRpcServiceImplTest.java
+++ service/src/test/java/com/hualala/app/wechat/impl/card/CardSyncRpcServiceImplTest.java
@@ -19,7 +19,7 @@ public class CardSyncRpcServiceImplTest extends BaseRpcTest {
         CardSyncRpcService.CardListReqData cardListReqData = new CardSyncRpcService.CardListReqData();
         cardListReqData.setCount(10);
         cardListReqData.setOffset(0);
-        cardListReqData.setMpID("doulaofangceshi");
+        cardListReqData.setMpID("hualala_com");
         List<String> cardStatusEnums = new ArrayList<>();
         cardStatusEnums.add(CardStatusEnum.CARD_STATUS_NOT_VERIFY.name());
         cardStatusEnums.add(CardStatusEnum.CARD_STATUS_VERIFY_FAIL.name());
@@ -54,9 +54,9 @@ public class CardSyncRpcServiceImplTest extends BaseRpcTest {
     public void testDonwloadCard() {
         CardSyncRpcService rpcClient = super.baseRpcClient.getRpcClient(CardSyncRpcService.class);
         CardSyncRpcService.CardDownloadReqData cardDownloadReqData = new CardSyncRpcService.CardDownloadReqData();
-        cardDownloadReqData.setCardKey("sffg7896aerzcvgadsg123");
-        cardDownloadReqData.setCardID("p7FjEuOpIEX6v_IU8-nAC_8SadM4");
-        cardDownloadReqData.setMpID("doulaofangceshi");
+        cardDownloadReqData.setCardKey("222sffg7896aerzcvgadsg123");
+        cardDownloadReqData.setCardID("pACwGs5bzfYrLQw3aMXrhr_30RVg");
+        cardDownloadReqData.setMpID("hualala_com");
         CardSyncRpcService.CardDownloadResData cardDownloadResData = rpcClient.downloadCardInfo(cardDownloadReqData);
         System.out.println(cardDownloadResData.getMessage());
     }
@@ -66,8 +66,8 @@ public class CardSyncRpcServiceImplTest extends BaseRpcTest {
         CardSyncRpcService rpcClient = super.baseRpcClient.getRpcClient(CardSyncRpcService.class);
         CardSyncRpcService.CardListReqData cardListReqData = new CardSyncRpcService.CardListReqData();
         cardListReqData.setCount(10);
-        cardListReqData.setOffset(30);
-        cardListReqData.setMpID("doulaofangceshi");
+        cardListReqData.setOffset(0);
+        cardListReqData.setMpID("hualala_com");
         List<String> cardStatusEnums = new ArrayList<>();
         cardStatusEnums.add(CardStatusEnum.CARD_STATUS_NOT_VERIFY.name());
         cardStatusEnums.add(CardStatusEnum.CARD_STATUS_VERIFY_FAIL.name());
@@ -79,7 +79,7 @@ public class CardSyncRpcServiceImplTest extends BaseRpcTest {
         List<String> cardIdList = cardList.getCardIdList();
         CardSyncRpcService.CardDownloadReqData cardDownloadReqData = new CardSyncRpcService.CardDownloadReqData();
         UUID uuid = UUID.randomUUID();
-        cardDownloadReqData.setMpID("doulaofangceshi");
+        cardDownloadReqData.setMpID("hualala_com");
         int i = 1;
         for (String cardID : cardIdList) {
             cardDownloadReqData.setCardKey(uuid.toString()+i++);
