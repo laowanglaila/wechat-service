9fbd91acfb6d9f8d154f7ece748020e2976b291c
任鉴非 <renjianfei@hualala.com>
2017-09-21 13:56:48 +0800

    创建mpID和shopIDs关系表， 单元测试

--- service/src/test/java/com/hualala/app/wechat/impl/mp/MpShopsRpcServiceImplTest.java
+++ service/src/test/java/com/hualala/app/wechat/impl/mp/MpShopsRpcServiceImplTest.java
@@ -15,10 +15,10 @@ public class MpShopsRpcServiceImplTest extends BaseRpcTest {
     public void test() throws InterruptedException {
         MpShopsRpcService rpcClient = super.baseRpcClient.getRpcClient( MpShopsRpcService.class );
         MpShopsRpcService.BindShopsReqData bindShopsReqData = new MpShopsRpcService.BindShopsReqData();
-        bindShopsReqData.setGroupID( 123L );
+        bindShopsReqData.setGroupID( 1123L );
         bindShopsReqData.setMpID( "123" );
         ArrayList <Long> objects = new ArrayList <>();
-        objects.add( 123L );
+        objects.add( 1213L );
         bindShopsReqData.setShopIDs( objects );
         MpShopsRpcService.BindShopsResData bindShopsResData = rpcClient.bindShops( bindShopsReqData );
         Assert.isTrue( bindShopsResData.getCode().equals( ErrorCodes.WECHAT_SUCCESS_CODE ) );
