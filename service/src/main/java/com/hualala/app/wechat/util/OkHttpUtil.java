da9f329ca90fe6e78d6dd74b6956f276b5e8bdf9
任鉴非 <renjianfei@hualala.com>
2017-10-10 14:31:05 +0800

    OK-http,异步请求接收一个函数接口

--- service/src/main/java/com/hualala/app/wechat/util/OkHttpUtil.java
+++ service/src/main/java/com/hualala/app/wechat/util/OkHttpUtil.java
@@ -5,9 +5,12 @@ import com.hualala.app.wechat.common.WechatExceptionTypeEnum;
 import com.hualala.app.wechat.exception.WechatException;
 import lombok.extern.slf4j.Slf4j;
 import okhttp3.*;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
-import java.util.concurrent.*;
+import java.net.SocketTimeoutException;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Consumer;
 
 /**
  * Created by renjianfei on 2017/8/23.
@@ -49,6 +52,18 @@ public class OkHttpUtil {
                 .build();
         client.newCall(request).enqueue(callback);
     }
+    /**
+     * 异步get请求
+     *
+     * @param url
+     * @param onResponse
+     */
+    public static void get(String url, Consumer<Response> onResponse) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        client.newCall(request).enqueue(new DefaultCallback( onResponse ));
+    }
 
 
     /**
@@ -87,6 +102,24 @@ public class OkHttpUtil {
         client.newCall(request).enqueue(callback);
     }
     /**
+     * 异步post请求,实现方法onResponse，提供默认的onFailure方法
+     *
+     * @param url
+     * @param json
+     * @param onResponse
+     * @throws IOException
+     */
+    public static void post(String url, String json, Consumer<Response> onResponse) {
+
+        RequestBody body = RequestBody.create(JSON, json);
+        Request request = new Request.Builder()
+                .url(url)
+                .post(body)
+                .build();
+        client.newCall(request).enqueue(new DefaultCallback(onResponse));
+    }
+
+    /**
      * 发送http请求
      * @param request
      * @return
@@ -107,5 +140,41 @@ public class OkHttpUtil {
             throw new WechatException(WechatExceptionTypeEnum.WECHAT_HTTP_FAILED, e.getMessage());
         }
         return jsonObject;
+    }
+    static class DefaultCallback implements Callback{
+
+        private int maxRetryTimes = 3;
+        String RETRY_NAME = "retryTimes";
+        private Consumer<Response> onResponse;
+        DefaultCallback(Consumer<Response> onResponse){
+            this.onResponse = onResponse;
+        }
+        @Override
+        public void onFailure(Call call, IOException e) {
+            Request request = call.request();
+            String header = request.header( RETRY_NAME );
+            Integer retryTimes;
+            if (StringUtils.isBlank( request.header( RETRY_NAME ) )) {
+                retryTimes = 0;
+            }else {
+                retryTimes = Integer.parseInt( header );
+            }
+            if(e.getCause().equals(SocketTimeoutException.class) && retryTimes < maxRetryTimes)//如果超时并未超过指定次数，则重新连接
+            {
+                retryTimes = retryTimes + 1;
+                request.newBuilder().addHeader( RETRY_NAME,retryTimes.toString() );
+                client.newCall(call.request()).enqueue(this);
+            }else {
+                log.error( "OkHttp异步请求发生异常-Request:"+request.toString()+"" );
+                log.error( "OkHttp异步请求发生异常-RequestBody:{}",JSONObject.toJSONString( request.body()));
+                log.error( "OkHttp异步请求发生异常-Exception:",e);
+            }
+        }
+
+        @Override
+        public void onResponse(Call call, Response response) throws IOException{
+            onResponse.accept( response );
+        }
+
     }
 }
