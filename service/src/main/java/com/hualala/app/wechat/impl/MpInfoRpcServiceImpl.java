249b7e7e47de77f109389655f9bd9619af8ad7a9
任鉴非 <renjianfei@hualala.com>
2018-01-07 12:26:49 +0800

    修复查询mpInfo接口

--- service/src/main/java/com/hualala/app/wechat/impl/MpInfoRpcServiceImpl.java
+++ service/src/main/java/com/hualala/app/wechat/impl/MpInfoRpcServiceImpl.java
@@ -1,6 +1,7 @@
 package com.hualala.app.wechat.impl;
 
 import com.hualala.app.wechat.MpInfoRpcService;
+import com.hualala.app.wechat.MpTypeEnum;
 import com.hualala.app.wechat.mapper.WechatMpMapper;
 import com.hualala.app.wechat.model.WechatMpModel;
 import com.hualala.core.utils.DataUtils;
@@ -11,6 +12,8 @@ import org.springframework.stereotype.Service;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by xkia on 2017/3/20.
@@ -46,13 +49,53 @@ public class MpInfoRpcServiceImpl implements MpInfoRpcService {
         if(reqData.getShopID() > 0){
             param.put("shopID", reqData.getShopID());
         }
-
+        if (reqData.getPageSize() == 0){
+            param.put( "pageSize",10 );
+        }else {
+            param.put( "pageSize",reqData.getPageSize() );
+        }
+        if (reqData.getPageNo() == 0){
+            param.put( "offset",0 );
+        }else {
+            param.put( "offset",reqData.getPageSize() * (reqData.getPageNo() - 1) );
+        }
 
         List<WechatMpModel> list = wechatMpMapper.queryList(param);
-        List<MpInfoResData> mpInfoLst = DataUtils.copyList(list, MpInfoResData.class);
+        List <MpInfoResData> collect = copyList( list );
         MpInfoQueryResData mpInfoQueryResData = new MpInfoQueryResData();
-        mpInfoQueryResData.setMpInfoResDataList(mpInfoLst);
+        mpInfoQueryResData.setMpInfoResDataList(collect);
         return mpInfoQueryResData;
+    }
+
+    private List <MpInfoResData> copyList(List <WechatMpModel> list) {
+        return list.stream().map( model -> {
+            MpInfoResData mpInfoResData = new MpInfoResData();
+            mpInfoResData.setMpID( model.getMpID() );
+            mpInfoResData.setGhID( model.getGhID() );
+            mpInfoResData.setMpName( model.getMpName() );
+            mpInfoResData.setMpType( MpTypeEnum.valueOf( model.getMpType() ) );
+            mpInfoResData.setToken( model.getToken() );
+            mpInfoResData.setAppID( model.getAppID() );
+            mpInfoResData.setAppSecret( model.getAppSecret() );
+            mpInfoResData.setEncodingAESKey( model.getEncodingAESKey() );
+            mpInfoResData.setWeixinURL( model.getWeixinURL() );
+            mpInfoResData.setHeadImg( model.getHeadImg() );
+            mpInfoResData.setQrCodeURL( model.getQrCodeURL() );
+            mpInfoResData.setMenuJson( model.getMenuJson() );
+            mpInfoResData.setGroupID( model.getGroupID());
+            mpInfoResData.setBrandID( model.getBrandID() );
+            mpInfoResData.setShopID( model.getShopID() );
+            mpInfoResData.setCustomerWithoutBindMobile( model.getCustomerWithoutBindMobile().toString() );
+            mpInfoResData.setSubscribeToCcustomer( model.getSubscribeToCcustomer().toString() );
+            mpInfoResData.setTableMsgTemplate( model.getTableMsgTemplate() );
+            mpInfoResData.setAlias( model.getAlias() );
+            mpInfoResData.setAuthorize( model.getAuthorize().toString() );
+            mpInfoResData.setAuthorizerRefreshToken( model.getAuthorizerRefreshToken() );
+            mpInfoResData.setOauth( model.getOauth().toString() );
+            mpInfoResData.setWechatEndDate( model.getWechatEndDate() );
+            mpInfoResData.setIsActiveUse( model.getIsActiveUse().toString() );
+            return mpInfoResData;
+        } ).collect( Collectors.toList() );
     }
 
     @Override
